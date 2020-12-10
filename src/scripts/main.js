6b4cce6aa7a23e4d3b8e01440c3c5da7237dc3cb
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2020-12-10 13:00:37 +0530

    added few useful js libraries and bootstrap grid

---
+++ src/scripts/main.js
@@ -0,0 +1,11 @@
+class SomeClass {
+    method() {
+        console.log('I\'m here!');
+    }
+}
+
+const test = new SomeClass;
+
+test.method();
+
+const swup = new Swup(); // only this line when included with script tag
